5df6a1bccd2557bebf8fe12d0ee349f238a96fd8
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2025-10-20 21:43:24 +0300

    Update deploy config and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/WardSadiehPortfolio/', // 👈 must match repo name exactly (case-sensitive)
   plugins: [react()],
 })
